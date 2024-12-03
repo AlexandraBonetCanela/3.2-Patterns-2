ad6e1efd630d7e9eef57425bcedb4347a4f8ba5f
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-12-03 18:47:11 +0100

    finished HawaianBuilder

---
+++ Nivell1-Builder/src/main/java/org/example/HawaianaBuilder.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.example.enums.Dough;
+import org.example.enums.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HawaianaBuilder implements PizzaBuilder{
+
+    private Size size;
+    private Dough dough;
+    private List<String> toppings = new ArrayList<>();
+
+    public HawaianaBuilder() {
+        toppings.add("tomato sauce");
+        toppings.add("mozarella");
+        toppings.add("pineapple");
+        toppings.add("ham");
+        toppings.add("onion");
+    }
+
+    @Override
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public void setDoughType(Dough doughType) {
+        this.dough = doughType;
+    }
+
+    @Override
+    public void setToppings(List<String> toppings){
+        this.toppings.addAll(toppings);
+    }
+
+    @Override
+    public Pizza build() {
+        return new Pizza(size, dough, toppings);
+    }
+}
